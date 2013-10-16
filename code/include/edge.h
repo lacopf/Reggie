da4b671b2c70ad3381c5ff2c10973e05b0b6eee8
lacopf <lacopf@ubuntu.ubuntu-domain>
2013-10-16 14:33:20 -0400

    please

--- code/include/edge.h
+++ code/include/edge.h
@@ -1,16 +1,29 @@
+#ifndef _edge_h_
+#define _edge_h_
 #include "node.h"
 
-public Edge{
-  private:
-  Node* node1;
-  Node* node2;
-  string realtion;
-  
-  public:
-  Edge();
-  Edge(Node* n1, Node* n2, string relation);
-  
-  string get_relation() {return relation;}
-  
+class Edge{
+	private:
+	  Node* node1;
+	  Node* node2;
+	  string relation;
+	  
+	public:
+	  Edge();
+	  Edge(Node* n1, Node* n2, string relation);
+	  
+	  string get_relation() {return relation;}
   
 };
+
+Edge::Edge()
+{
+	node1 = NULL;
+	node2 = NULL;
+	relation = "";
+}
+
+
+
+
+#endif
--- code/include/node.h
+++ code/include/node.h
@@ -1,21 +1,68 @@
+
+#ifndef _node_h_
+#define _node_h_
+
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-public Node{
-private:
-  string information;
-  int index;
-  Node* parent;
-  
-public:
-  int inline get_index() {return index;}
-  string inline get_information() {return information;}
-  Node* inline get_parent() {return parent;}
-  
-  Node();
-  Node(string info, int ind, Node* parent);
-  
-  
+#include "edge.h"
 
+class Node{
+	private:
+	  string information;
+	  vector<string> tags;
+	  int index;
+	  Node* parent;
+	  vector<Edge*> edges;
+	  
+	public:
+	  int inline get_index() {return index;}
+	  string inline get_information() {return information;}
+	  Node* inline get_parent() {return parent;}
+	  
+	  Node();
+	  Node(string info, int ind, Node* par);
+
+	  void inline add_edge(Edge* edg) {edges.push_back(edg);}
+	  void inline add_tag(string tag) {tags.push_back(tag);}
+
+	  bool has_tag(string tag);
+  
 };
+
+Node::Node()
+{
+	vector<string> tempt;
+	vector<Edge*> tempe;
+
+	tags = tempt;
+	edges = tempe;
+	information = "";
+	index = -1;
+	parent = NULL;
+}
+Node::Node(string info, int ind, Node* par)
+{
+	vector<string> tempt;
+	vector<Edge*> tempe;
+
+	tags = tempt;
+	edges = tempe;
+	information = info;
+	index = ind;
+	parent = par;
+}
+Node::has_tag(string tag)
+{
+	for (int i = 0; i < tags.size(); i++)
+		if (tags[i].compare(tag) == 0)
+			return true;
+
+	return false;
+}
+
+#endif
+
+
