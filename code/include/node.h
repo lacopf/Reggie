4a51a68c83b150b60e9728e6444f5e6fb6e7c3d7
lacopf <lacopf@rpi.edu>
2013-10-16 17:16:15 -0400

    Create node.h
    Basic node class

---
+++ code/include/node.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+public Node{
+private:
+  string information;
+  int index;
+  Node* parent;
+  
+public:
+  int inline get_index() {return index;}
+  string inline get_information() {return information;}
+  Node* get_parent() {return parent;}
+  
+  
+
+
+};
