12da1c5040d17141eb020e5ee6f6103e2ed42a42
Viktor <viktor@ubuntu.(none)>
2013-10-16 17:59:21 -0400

    Created Graph Skeleton

--- code/include/graph.h
+++ code/include/graph.h
@@ -1,12 +1,25 @@
-#IFNDEF _GRAPH
-#DEFINE _GRAPH
+#ifndef _GRAPH
+#define _GRAPH
 
-class graph
+#include "node.h"
+#include "edge.h"
+#include <vector>
+
+using namespace std;
+
+class Graph
 {
 	public:
-	
-	private:
-	
-}
+		vector<Node> getNodes(){ return nodes; }
+		vector<Edge> getEdges(){ return edges; }
 
-#ENDIF
+		void addNode(Node node){ nodes.push_back(node); }
+		void addEdge(Edge edge){ edges.push_back(edge); }
+
+		
+	private:
+		vector<Node> nodes;
+		vector<Edge> edges;
+};
+
+#endif
