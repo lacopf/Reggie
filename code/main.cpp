513af6150ffb0590833e611d23d3cd23eb9561e3
Wesley Miller <millew2@rpi.edu>
2013-10-30 17:53:43 -0400

    coordinat system fix

--- code/main.cpp
+++ code/main.cpp
@@ -65,6 +65,7 @@ int main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_DOUBLE);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
+	gluOrtho2D(0, 500, 0, 500);
 	glutCreateWindow("JungleReggie");
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(resize);
