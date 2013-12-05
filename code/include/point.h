256a806eef6dc44905269ac4f81349fab446004a
Ben Caulfield <caulfb2@rpi.edu>
2013-12-05 14:13:19 -0500

    Put code into standard format
    added inline keyword to methods

--- code/include/point.h
+++ code/include/point.h
@@ -9,32 +9,32 @@ class Point
 		bool down;//if it was down click
 
 	public:
-		Point()//default constructor
+		inline Point()//default constructor
 		{
 			x = -1;
 			y = -1;
 			down = false;
 		}
-		Point(double i, double j, bool press)//constructor
+		inline Point(double i, double j, bool press)//constructor
 		{
 			x = i;
 			y = j;
 			down = press;
 		}
-		int getX()//get x coordinate
+		inline int getX()//get x coordinate
 		{
 			return x;
 		}
-		int getY()//get y coordinate
+		inline int getY()//get y coordinate
 		{
 			return y;
 		}
-		bool getDown()//get down and boogie
+		inline bool getDown()//get down and boogie
 		{
 			return down;
 		}
 
-		void move(int newX, int newY)//lets you move a node after you've already placed it
+		inline void move(int newX, int newY)//lets you move a node after you've already placed it
 		{
 			x = newX;
 			y = newY;
